ae730ac2753db1e86d3bc345d3f9f28a3ddd904f
a130888599 <46438185+a130888599@users.noreply.github.com>
2019-04-08 01:04:50 +0800

    have some bugs

---
+++ week3/代码及头文件/AQueue.c
@@ -0,0 +1,220 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#include"AQueue.h"
+int Type;       //记录选择的数据类型
+int k = 0;      //判断是否已经初始化
+
+/**
+ *  @name        : void InitAQueue(AQueue *Q)
+ *    @description : 初始化队列
+ *    @param         Q 队列指针Q
+ *  @notice      : None
+ */
+void InitAQueue(AQueue *Q)
+{
+    Type = getType();
+    k = 1;
+    Q->front = Q->rear = Q->length = 0;
+}
+
+/**
+ *  @name        : void DestoryAQueue(AQueue *Q)
+ *    @description : 销毁队列
+ *    @param         Q 队列指针Q
+ *  @notice      : None
+ */
+void DestoryAQueue(AQueue *Q)
+{
+    if(!k)
+    {
+        printf("未初始化列队！\n");
+        return ;
+    }
+
+   ClearAQueue(Q);
+   k = 0;
+   printf("销毁成功\n");
+}
+
+
+/**
+ *  @name        : Status IsFullAQueue(const AQueue *Q)
+ *    @description : 检查队列是否已满
+ *    @param         Q 队列指针Q
+ *    @return         : 满-TRUE; 未满-FALSE
+ *  @notice      : None
+ */
+Status IsFullAQueue(const AQueue *Q)
+{
+    if ((Q->rear + 1) % MAXQUEUE == Q->front)
+	{
+		printf("队列已满！\n");
+		return TRUE;
+	}
+	return FALSE;
+}
+
+/**
+ *  @name        : Status IsEmptyAQueue(const AQueue *Q)
+ *    @description : 检查队列是否为空
+ *    @param         Q 队列指针Q
+ *    @return         : 空-TRUE; 未空-FALSE
+ *  @notice      : None
+ */
+Status IsEmptyAQueue(const AQueue *Q)
+{
+    if (!Q->length)
+	{
+		printf("队列为空！\n");
+		return TRUE;
+	}
+	return FALSE;
+}
+
+
+/**
+ *  @name        : Status GetHeadAQueue(AQueue *Q, void *e)
+ *    @description : 查看队头元素
+ *    @param         Q 队列指针Q，存放元素e
+ *    @return         : 成功-TRUE; 失败-FALSE
+ *  @notice      : 队列是否空
+ */
+Status GetHeadAQueue(AQueue *Q, void *e)
+{
+    if (!k)
+    {
+        printf("请先初始化队列\n");
+        return FALSE;
+    }
+    if (IsEmptyAQueue(Q))
+        return FALSE;
+
+    memcpy(e, Q->data[Q->front], getSize(Type));
+    return TRUE;
+}
+
+
+/**
+ *  @name        : int LengthAQueue(AQueue *Q)
+ *    @description : 确定队列长度
+ *    @param         Q 队列指针Q
+ *    @return         : 队列长度count
+ *  @notice      : None
+ */
+int LengthAQueue(AQueue *Q)
+{
+    if(!k)
+    {
+        printf("请先初始化队列！\n");
+        return FALSE;
+    }
+
+    return Q->length;
+}
+
+
+/**
+ *  @name        : Status EnAQueue(AQueue *Q, void *data)
+ *    @description : 入队操作
+ *    @param         Q 队列指针Q,入队数据指针data
+ *    @return         : 成功-TRUE; 失败-FALSE
+ *  @notice      : 队列是否满了或为空
+ */
+Status EnAQueue(AQueue *Q, void *data)
+{
+    if(!k)
+    {
+        printf("请先初始化队列！\n");
+        return FALSE;
+    }
+    if(IsFullAQueue(Q))
+        return FALSE;
+
+    Q->data[Q->rear] = malloc(getSize(Type));
+    memcpy(Q->data[Q->rear], data, getSize(Type));
+    Q->rear = (Q->rear + 1) % MAXQUEUE;
+    Q->length++;
+    return TRUE;
+}
+
+
+/**
+ *  @name        : Status DeAQueue(AQueue *Q)
+ *    @description : 出队操作
+ *    @param         Q 队列指针Q
+ *    @return         : 成功-TRUE; 失败-FALSE
+ *  @notice      : None
+ */
+Status DeAQueue(AQueue *Q)
+{
+    if(!k)
+    {
+        printf("请先初始化队列！\n");
+        return FALSE;
+    }
+    if(IsEmptyAQueue(Q))
+        return FALSE;
+
+    free(Q->data[Q->front]);
+    Q->front = (Q->front + 1) % MAXQUEUE;
+    Q->length--;
+    printf("出队成功！\n");
+    return TRUE;
+}
+
+
+/**
+ *  @name        : void ClearAQueue(Queue *Q)
+ *    @description : 清空队列
+ *    @param         Q 队列指针Q
+ *  @notice      : None
+ */
+void ClearAQueue(AQueue *Q)
+{
+    if (!k)
+    {
+        printf("请先初始化队列！\n");
+        return ;
+    }
+    Q->front = Q->rear = Q->length = 0;
+}
+
+
+/**
+ *  @name        : Status TraverseAQueue(const AQueue *Q, void (*foo)(void *q))
+ *    @description : 遍历函数操作
+ *    @param         Q 队列指针Q，操作函数指针foo
+ *    @return         : None
+ *  @notice      : None
+ */
+Status TraverseAQueue(const AQueue *Q, void (*foo)(void *q))
+{
+    if(!k)
+    {
+        printf("请先初始化队列！\n");
+        return FALSE;
+    }
+    if (IsEmptyAQueue(Q))
+        return FALSE;
+
+    int p = Q->front;
+    while(p < Q->length + Q->front)
+        foo(Q->data[p++ % MAXQUEUE]);
+    return TRUE;
+}
+
+
+/**
+ *  @name        : void APrint(void *q)
+ *    @description : 操作函数
+ *    @param         q 指针q
+ *  @notice      : None
+ */
+void APrint(void *q)
+{
+    if(Type == 1)
+        printf("%d  ", *(int *)q);
+    if(Type == 2)
+        printf("%.3f  ", *(float *)q);
+}
---
+++ week3/代码及头文件/main.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#include"AQueue.h"
+int size;   //size为判断数据类型
+
+int main()
+{
+    int *I; float *F; char *C; void *data;
+	int flag = 1;
+	AQueue Q;
+	while (flag)
+	{
+		system("cls");
+		welcome();
+		int choose = bool_choose();
+		system("cls");
+		switch(choose)
+		{
+		    case 1: InitAQueue(&Q);wait();break;
+		    case 2: DestoryAQueue(&Q);wait();break;
+		    case 3: ClearAQueue(&Q);printf("清除成功!\n");wait();break;
+		    case 4: printf("%d", LengthAQueue(&Q));wait();break;
+		    case 5: enter_queue(&Q, data);wait();break;
+		    case 6: DeAQueue(&Q);wait();break;
+		    case 7: TraverseAQueue(&Q, APrint);wait();break;
+		    case 8: exit(0);break;
+		    default: printf("输入错误\n");
+		}
+	}
+}
+
+void welcome()
+{
+	printf("------------------------------------------\n");
+	printf("------------\n");
+	printf("---请选择功能:\n");
+	printf("---1.初始化队列\n");
+	printf("---2.销毁队列\n");
+	printf("---3.清空队列\n");
+	printf("---4.队列长度\n");
+	printf("---5.入队\n");
+	printf("---6.出队\n");
+	printf("---7.打印队列\n");
+	printf("---8.退出\n");
+	printf("------------\n");
+	printf("------------------------------------------\n\n");
+}
+
+int bool_choose()
+{
+    printf("请选择功能：");
+    int k;
+    while(1)
+    {
+        int flag = 1;
+        scanf("%d", &k);
+        if(k < 0 || k > 8)
+            flag = 0;
+
+        if(!flag)
+        {
+            printf("输入错误，请重新输入\n");
+            continue;
+        }
+        break;
+    }
+}
+
+int getType()
+{
+    int type, flag = 0;
+    while(!flag)
+    {
+        system("cls");
+        int choose;
+        printf("请选择数据类型：\n");
+        printf("1.int\n2.float\n");
+        scanf("%d", &choose);
+        switch(choose)
+        {
+            case 1: type = 1; flag = 1; break;
+            case 2: type = 2; flag = 1; break;
+            default : printf("请重新输入\n");
+        }
+    }
+    size = type;
+    return type;
+}
+
+int getSize(int type)
+{
+    int type_size;
+
+    if(type == 1)
+        type_size = sizeof(int);
+    if(type == 2)
+        type_size = sizeof(float);
+
+    return type_size;
+}
+
+void *getKey(int type)
+{
+    void *key = NULL;
+    int *I = (int*)malloc(sizeof(int));
+    float *F = (float*)malloc(sizeof(float));
+
+    printf("请输入数据\n");
+    switch (type)
+    {
+        case 1:    scanf("%d", I);
+                        key = (void *)I; break;
+
+        case 2:    scanf("%f", F);
+                        key = (void *)F; break;
+        default:    printf("ERROR\n");
+    }
+    return key;
+}
+
+void enter_queue(AQueue *Q, void *data)     //入队
+{
+    printf("请问要输入多少个数据：");
+    int times = 0;
+    scanf("%d", &times);
+
+    while(times--)
+    {
+        printf("剩余%d个数据\n", times + 1);
+        data = getKey(size);
+        if(!EnAQueue(Q, data))
+                                break;
+    }
+}
+
+void wait()
+{
+    getchar();
+    getchar();
+}
